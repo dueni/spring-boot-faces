691d07cab17875c08ecf3a5eb8c39e473c650355
H. Duennenberger <hampidu@gmail.com>
2024-05-03 13:45:00 +0200

    working with spring-boot 2.7 on java 11

--- src/main/java/com/auth0/samples/bootfaces/SystemInfo.java
+++ src/main/java/com/auth0/samples/bootfaces/SystemInfo.java
@@ -3,9 +3,13 @@ package com.auth0.samples.bootfaces;
 import org.ocpsoft.rewrite.el.ELBeanName;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Component(value = "info")
 @ELBeanName(value = "info")
 public class SystemInfo {
+	private static final DateTimeFormatter NOW_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.S");
 
 	public String getJavaVersion() {
 		return System.getProperty("java.version");
@@ -13,5 +17,9 @@ public class SystemInfo {
 
 	public String getJavaHome() {
 		return System.getProperty("java.home");
+	}
+
+	public String getNow() {
+		return LocalDateTime.now().format(NOW_FORMAT);
 	}
 }
